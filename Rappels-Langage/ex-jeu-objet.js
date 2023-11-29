e2617dfbaa692091b99bf2fea9b08cd6e5e102c7
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-11-29 12:41:00 +0100

    Correction

--- Rappels-Langage/ex-jeu-objet.js
+++ Rappels-Langage/ex-jeu-objet.js
@@ -1,3 +1,6 @@
+const readline = require('readline');
+const process = require('process');
+
 // Reprendre le code de ex-jeu.js
 
 // Exercice 1
@@ -5,6 +8,25 @@
 // Créer un namespace object Random qui regroupe
 // les 4 fonctions (de la correction)
 // On écrira Random.getRandomInt(0, 100) pour l'appel
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+}
+
 
 // Exercice 2
 // Créer un classe Jeu dont le constructeur
@@ -17,6 +39,49 @@
 // A ce stade le constructeur ne prend pas de paramètre
 // const game = new Jeu();
 // game.jouer()
+
+class Jeu {
+  constructor(options = {}) {
+    const min = options.min ?? 0;
+    const max = options.max ?? 100;
+    this.rl = readline.createInterface({
+      input: process.stdin, // le clavier
+      output: process.stdout, // le terminal
+    });
+    this.entierAlea = Random.getRandomInt(min, max);
+    this.essais = [];
+  }
+  jouer() {
+    if (this.essais.length) {
+      console.log(`Vous avez déjà joué : ${this.essais.join(' - ')}`);
+    }
+    this.rl.question('Quel est le nombre ? ', (answer) => {
+
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Erreur : il faut saisir un entier');
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        this.jouer();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        this.jouer();
+      } else {
+        console.log('Gagné');
+        this.rl.close();
+      }
+    });
+  }
+}
+
+const game = new Jeu();
+game.jouer()
 
 // Exercice 3
 // Modifier le constructeur de Jeu
