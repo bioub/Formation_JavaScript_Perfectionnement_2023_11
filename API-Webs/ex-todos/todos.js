0b223c292a7fc691c585bfe7ccd986fc6d2dc6be
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-11-30 15:45:52 +0100

    Correction partielle

--- API-Webs/ex-todos/todos.js
+++ API-Webs/ex-todos/todos.js
@@ -20,6 +20,10 @@ export function createTodoItem(todo) {
   // Exercice 1
   // Ajouter la checkbox
   // et la cocher si todo.completed vaut true
+  const checkboxEl = document.createElement('input');
+  checkboxEl.type = 'checkbox';
+  checkboxEl.className = 'todo-completed';
+  checkboxEl.checked = todo.completed;
 
   const spanEl = document.createElement('span');
   spanEl.className = 'todo-title';
@@ -34,8 +38,14 @@ export function createTodoItem(todo) {
   // Exercice 2
   // Ajouter le bouton moins
   // Ecouter son click et supprimer la balise div
+  const buttonEl = document.createElement('button');
+  buttonEl.className = "todo-btn-delete";
+  buttonEl.innerText = '-';
+  buttonEl.addEventListener('click', () => {
+    divEl.remove();
+  })
 
-  divEl.append(spanEl);
+  divEl.append(checkboxEl, ' ', spanEl, ' ', buttonEl);
 
   return divEl;
 }
